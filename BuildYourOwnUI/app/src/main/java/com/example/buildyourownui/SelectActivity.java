900814dc03b74c02e642fa7bc830048fc6df5a3c
frvzs <109150497+frvzs@users.noreply.github.com>
2023-02-23 21:11:44 +0100

    Changes in SelectActivity

--- BuildYourOwnUI/app/src/main/java/com/example/buildyourownui/SelectActivity.java
+++ BuildYourOwnUI/app/src/main/java/com/example/buildyourownui/SelectActivity.java
@@ -32,30 +32,7 @@ public class SelectActivity extends AppCompat {
         // Start playing the music
         //mediaPlayer.start();
 
-
-
-        // Open Main Menu
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // Query: Username entered
-                if (editUser.getText().toString().isEmpty()==false) {
-                    // username entered
-                    launchMainActivity();
-                }
-                else{
-                    // no username entered
-                    lblError.setVisibility(View.VISIBLE);
-                }
-
-                //save Username
-                String username = editUser.getText().toString();
-                usernameManager.setUser(username);
-            }
-        });
-
-    }
+        };
 
     private void launchMainActivity(){
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
