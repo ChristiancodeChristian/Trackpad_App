556bcf5c9730927cec595d32ae1f2c1e82cb7ec9
Elena <e.knollhuber@mci4me.at>
2023-02-24 21:38:23 +0100

    int to string nochmal

--- BuildYourOwnUI/app/src/main/java/com/example/buildyourownui/GameActivity.java
+++ BuildYourOwnUI/app/src/main/java/com/example/buildyourownui/GameActivity.java
@@ -170,22 +170,15 @@ public class GameActivity extends AppCompat {
     }
     private void incrementCounter() {
 
-<<<<<<< Updated upstream
-        mCounter = mCounter++;
-        //mCounterText.setText("Counter: " + mCounter);
-        scorecounter.setText(mCounter);
-        if (mCounter >= 25) {
-=======
         mCounter = 1;
         //mCounterText.setText("Counter: " + mCounter);
         scorecounter.setText(String.valueOf(mCounter));
 
-/*        if (mCounter >= 25) {
->>>>>>> Stashed changes
+        if (mCounter >= 25) {
             // Launch a new activity when counter reaches 25
             Intent intentFin = new Intent(GameActivity.this, ScoreActivity.class);
             startActivity(intentFin);
-        }*/
+        }
     }
     @Override
     protected void onDestroy() {
