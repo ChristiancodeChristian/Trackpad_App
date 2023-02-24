b40e016d8c1a7a5dee883828a153639cfc4eaf15
ChristiancodeChristian <mollchristian98@gmail.com>
2023-02-24 21:59:49 +0100

    1

--- BuildYourOwnUI/app/src/main/java/com/example/buildyourownui/GraphActivity.java
+++ BuildYourOwnUI/app/src/main/java/com/example/buildyourownui/GraphActivity.java
@@ -9,6 +9,8 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;*/
+
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
---
+++ BuildYourOwnUI/app/src/main/java/com/example/buildyourownui/GraphHelperActivity.java
@@ -0,0 +1,12 @@
+package com.github.mikephil.charting.charts;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+public class GraphHelperActivity {
+}
