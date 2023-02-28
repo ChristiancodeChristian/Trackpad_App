9667604a25a95727d04b2b18e7b88e92df064ad3
ChristiancodeChristian <mollchristian98@gmail.com>
2023-02-28 15:03:36 +0100

    sdfvgbhn

--- BuildYourOwnUI/app/src/main/java/com/example/buildyourownui/MQTTActivity.java
+++ BuildYourOwnUI/app/src/main/java/com/example/buildyourownui/MQTTActivity.java
@@ -16,7 +16,7 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-public class MQTTActivity extends AppCompatActivity {
+public class MQTTActivity extends AppCompat {
 
     MqttAndroidClient client;
     TextView subText;
